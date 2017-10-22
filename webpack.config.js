1f9ee03cd47b91c84dfb5f85a4819e80126d9b00
Zachary Kane Pride <kane.zach@gmail.com>
2017-10-22 11:53:02 -0700

    update webpack setup
    take things back to just webpacking the client side.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,86 @@
-module.exports = function (env) {
-    return require(`./webpack.${env}.js`);
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = function(env) {
+    const config = {
+        entry: {
+            main: './src/js/entry.js',
+            vendor: ['lodash'],
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'public'),
+        },
+        devtool: 'eval',
+        devServer: {
+            contentBase: './public',
+            host: '0.0.0.0',
+            watchOptions: {
+                poll: true,
+            },
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                },
+                {
+                    test: /\.scss$/,
+                    exclude: /node_modules/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                            },
+                            {
+                                loader: 'sass-loader',
+                            },
+                        ],
+                    }),
+                },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(['public']),
+            new ExtractTextPlugin({
+                filename: '[name].[contenthash].css',
+                disable: !!env.production,
+            }),
+            new HtmlWebpackPlugin({
+                title: 'Development',
+                template: './src/template.ejs',
+            }),
+            new webpack.HashedModuleIdsPlugin(),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor',
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'runtime',
+            }),
+        ],
+    };
+
+    if (env.production) {
+        config.output.filename = '[name].[chunkhash].js';
+        config.devtool = 'source-maps';
+        config.plugins.push(
+            new UglifyJSPlugin({
+                sourceMap: true,
+            }),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: JSON.stringify('production'),
+                },
+            }),
+        );
+    }
+
+    return config;
 };
